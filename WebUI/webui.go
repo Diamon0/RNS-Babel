5cf64369d89d973f67b98cc177feea2f6f3baaa4
Diamon0 <79779017+Diamon0@users.noreply.github.com>
2024-07-22 22:55:29 -0400

    [Feature] Initial Web UI Files

---
+++ WebUI/webui.go
@@ -0,0 +1,33 @@
+package webui
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+// I figured I'd make a web UI first, it's easier to maintain and contribute to
+// The plan is to make a Qt GUI later down the line
+
+var templates *template.Template
+const WEBDIR string = "WebUI/"
+
+func init() {
+    templates = template.Must(template.ParseFiles(WEBDIR+"templates/index.html"))
+}
+
+func StartWeb(addr string) error {
+    http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir(WEBDIR+"static"))))
+    http.HandleFunc("GET /", Home)
+
+    return http.ListenAndServe(addr, nil)
+}
+
+func Home(w http.ResponseWriter, r *http.Request) {
+    if err := templates.ExecuteTemplate(w, "base", nil); err != nil {
+        log.Println("Could not execute Home templates:", err)
+        http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+        return
+    }
+    return
+}
